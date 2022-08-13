2e6e8ebc8ab3a1778ae4736e6c3b9d77127d29c6
Lesley-Nicole <lesley-nicole@protonmail.com>
2022-08-13 18:53:15 -0500

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -7,20 +7,20 @@ export default function HeadComponent() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#000000" />
 
-      <title>Mythic Merchant</title>
+      <title>Mythic Market</title>
       <meta name="title" content="Mythic Market" />
       <meta name="description" content="Get mythical merchandise using Solana Pay!" />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://buildspace.so/@Lesley" />
+      <meta property="og:url" content="https://mythic-market-solana-pay.vercel.app" />
       <meta property="og:title" content="Mythic Market" />
       <meta property="og:description" content="Get mythical merchandise using Solana Pay!" />
       <meta property="og:image" content="/public/demo.jpg" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://twitter.com/lesley___nicole" />
+      <meta property="twitter:url" content="https://mythic-market-solana-pay.vercel.app" />
       <meta property="twitter:title" content="Mythic Market" />
       <meta property="twitter:description" content="Get mythical merchandise using Solana Pay!" />
       <meta property="twitter:image" content="/public/demo.jpg" />
